9f99569beba21a0dd1a91a745cfd0a63897d1b8a
danielsouza82 <65150676+danielsouza82@users.noreply.github.com>
2024-07-17 15:27:25 -0300

    Criado shared module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,14 @@ import { PrimeNGConfig } from 'primeng/api';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'stock-control';
 
   constructor(private primeNgConfig: PrimeNGConfig) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.primeNgConfig.ripple = true;
-}
-
+  }
 }
