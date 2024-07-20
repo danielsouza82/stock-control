62ea3ab87db4559c2d43a3a9e9e053661950a65a
danielsouza82 <65150676+danielsouza82@users.noreply.github.com>
2024-07-20 19:01:08 -0300

    Criado tela de produtos

---
+++ src/app/modules/products/page/product-home/product-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-home',
+  templateUrl: './product-home.component.html',
+  styleUrls: []
+})
+export class ProductHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/modules/products/products.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { ProductHomeComponent } from "./page/product-home/product-home.component";
+
+export const PRODUCT_ROUTES: Routes = [
+    {
+        path: '',
+        component: ProductHomeComponent
+    },
+];
